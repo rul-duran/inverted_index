2203cfffd53319220406456bc320db2e865f599b
rul-duran <raulduran816@gmail.com>
2023-07-17 13:45:59 -0600

    feat: added logging library

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from services.openalex import getRandomWork
 from utils.inverted_index import inverted_index_to_text
 
@@ -6,8 +8,9 @@ work = getRandomWork()
 
 # if the api returns a job with no inverse index, we get a new one
 while (not work['abstract_inverted_index']):
-    print('Inveted index is None, getting another work...')
+    logging.info('Inveted index is None, getting another work... \n')
     work = getRandomWork()
 
-# the inverted index is converted to its original text form
-print(inverted_index_to_text(work['abstract_inverted_index']))
+if __name__ == "__main__":
+    # the inverted index is converted to its original text form
+    print(inverted_index_to_text(work['abstract_inverted_index']))
